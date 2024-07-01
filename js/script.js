e60aa282cbe700843f3440939e02299c189ea24c
Fei <78798119+SilviaEera@users.noreply.github.com>
2024-07-01 10:05:57 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Add custom JavaScript here
+
+// const nav = document.getElementById('navBg');
+// window.onscroll = function () {
+//     if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 200 ) {
+//         nav.classList.add("bg-dark");
+//         // nav.classList.remove("bg-transparent");
+//     } else {
+//         // nav.classList.add(" bg-transparent");
+//         nav.classList.remove("bg-dark");
+//     }
+// }
+
+
+// $(document).ready(function() {
+//     $(".dropdown-toggle").dropdown();
+// });
+
+
+// var typed = new Typed(".element", {
+//         strings: ["Desiners", "Founders", "Developers"],
+//         smartBackspace: true,
+//         typeSpeed: 100,
+//         loop: true,
+//         loopCount: Infinity,
+//         startDelay: 100,
+//         backSpeed: 100,
+//       });
